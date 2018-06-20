983023a9a80083123951fa424ded953dc3b83cc0
Veronica Bustabad <vbustabad@gmail.com>
2018-06-20 14:17:08 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,6 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
+  GROUP BY projects.title;
   WHERE pledges.amount > projects.funding_goal";
 end
 
