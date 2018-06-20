1a1fd5356a2211b73fd614bc653d6ee4c4f3a85a
Veronica Bustabad <vbustabad@gmail.com>
2018-06-20 16:29:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects
   INNER JOIN pledges
-  ON projects.id = pledges.id
+  ON projects.id = pledges.project_id
   WHERE projects.category = 'music'";
 end
 
