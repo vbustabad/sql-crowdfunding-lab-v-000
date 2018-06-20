ca17319ebfe81d3b5d1c44b3182b1e3120619a5b
Veronica Bustabad <vbustabad@gmail.com>
2018-06-20 16:10:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,6 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, SUM(pledges.amount)
   FROM users
   INNER JOIN pledges
+  ON pledges.user_id = users.id
   GROUP BY users.name
   ORDER BY users.name, SUM(pledges.amount)";
 end
