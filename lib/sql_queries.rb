60cf71d221cebacf75355203e1860ecb9878724c
Veronica Bustabad <vbustabad@gmail.com>
2018-06-20 16:07:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,9 +34,9 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT users.name, SUM(pledges.amount)
   FROM users
-  INNER JOIN
+  INNER JOIN pledges
   GROUP BY users.name
-  ORDER BY "
+  ORDER BY users.name, SUM(pledges.amount)";
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
